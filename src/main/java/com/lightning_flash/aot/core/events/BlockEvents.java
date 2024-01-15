792cd83f0d12669c95f295f666b717fdc0e571e3
Zack-Fleming <dev.flemingzackery@gmail.com>
2024-01-15 18:40:24 -0500

    -1.19.4.01 build 123: added the ability for sponges to dry near 'hot blocks'. Also, added the ability for 'hot' blocks to dry nearby sponges. Lastly, added the ability to redye wool in crafting recipes, and undye wool when interacting with a full cauldron.

--- src/main/java/com/lightning_flash/aot/core/events/BlockEvents.java
+++ src/main/java/com/lightning_flash/aot/core/events/BlockEvents.java
@@ -18,10 +18,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SpongeBlock;
-import net.minecraft.world.level.block.WetSpongeBlock;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.SurfaceSystem;
 import net.minecraftforge.common.Tags;
@@ -38,6 +35,11 @@ import java.util.List;
 public class BlockEvents
 {
     private static final List<Block> HOT_BLOCKS = List.of(Blocks.LAVA, Blocks.LAVA_CAULDRON, Blocks.MAGMA_BLOCK, Blocks.FIRE);
+    private static final List<Item> WOOLEN_BLOCKS = List.of(
+            Blocks.ORANGE_WOOL.asItem(), Blocks.MAGENTA_WOOL.asItem(), Blocks.LIGHT_BLUE_WOOL.asItem(), Blocks.YELLOW_WOOL.asItem(),
+            Blocks.LIME_WOOL.asItem(), Blocks.PINK_WOOL.asItem(), Blocks.GRAY_WOOL.asItem(), Blocks.LIGHT_GRAY_WOOL.asItem(),
+            Blocks.CYAN_WOOL.asItem(), Blocks.PURPLE_WOOL.asItem(), Blocks.BLUE_WOOL.asItem(), Blocks.BROWN_WOOL.asItem(),
+            Blocks.GREEN_WOOL.asItem(), Blocks.RED_WOOL.asItem(), Blocks.BLACK_WOOL.asItem());
 
     @SubscribeEvent
     public static void rightClickBlock(PlayerInteractEvent.RightClickBlock event)
@@ -57,11 +59,21 @@ public class BlockEvents
                 if (held_item == Items.BONE)
                 {
                     int count = AOTMain.RANDOM.nextInt(0, 3);
-
-                    level.playSound(player,pos, SoundEvents.FLINTANDSTEEL_USE, SoundSource.BLOCKS,1.0f , AOTMain.RANDOM.nextFloat());
-
+                    level.playSound((Player) null, pos, SoundEvents.FLINTANDSTEEL_USE, SoundSource.BLOCKS,1.0f , (1.0F + level.random.nextFloat() * 0.2F) * 0.7F);
                     stack.setCount(stack.getCount() - 1);
                     player.drop(new ItemStack(ItemInit.SHARD_BONE.get().asItem(), count), true);
+                }
+            }
+
+            // wool undye event
+            else if (state.is(Blocks.WATER_CAULDRON))
+            {
+                if (WOOLEN_BLOCKS.contains(held_item))
+                {
+                    ((LayeredCauldronBlock) state.getBlock()).lowerFillLevel(state, level, pos);
+                    level.playSound((Player) null, pos, SoundEvents.PLAYER_SPLASH, SoundSource.BLOCKS, 1.0F, (1.0F + level.random.nextFloat() * 0.2F) * 0.7F);
+                    player.drop(new ItemStack(Items.WHITE_WOOL, 1), true);
+                    stack.setCount(stack.getCount() - 1);
                 }
             }
         }
@@ -73,8 +85,9 @@ public class BlockEvents
         BlockPos pos = event.getPos();
         LevelAccessor world = event.getLevel();
         Block block = world.getBlockState(pos).getBlock();
+        int lookDist = 7/2;
 
-
+        // sponge drying next to 'hot blocks'
         if (block instanceof WetSpongeBlock)
         {
 //            System.out.println("i am triggered!!!!");
@@ -86,7 +99,7 @@ public class BlockEvents
 //
 //            world.setBlock(new BlockPos(pos.getX(), pos.getY(), pos.getZ() + 1), Blocks.GREEN_WOOL.defaultBlockState(), 3);
 //            world.setBlock(new BlockPos(pos.getX(), pos.getY(), pos.getZ() - 1), Blocks.GREEN_WOOL.defaultBlockState(), 3);
-            int lookDist = 7/2;
+
             for (int h = -lookDist; h <= lookDist; h++)
             {
                 for (int j = -lookDist; j <= lookDist; j ++)
@@ -101,6 +114,28 @@ public class BlockEvents
                             world.levelEvent(2009, pos, 0);
                             world.playSound((Player) null, pos, SoundEvents.FIRE_EXTINGUISH, SoundSource.BLOCKS, 1.0F, (1.0F + world.getRandom().nextFloat() * 0.2F) * 0.7F);
                             return;
+                        }
+                    }
+                }
+            }
+        }
+
+        // 'hot blocks' drying sponges
+        else if (HOT_BLOCKS.contains(block))
+        {
+            for (int h = -lookDist; h <= lookDist; h++)
+            {
+                for (int j = -lookDist; j <= lookDist; j++)
+                {
+                    for (int i = -lookDist; i <= lookDist; i++)
+                    {
+                        BlockPos newPos = new BlockPos(pos.getX() + i, pos.getY() + j, pos.getZ() + h);
+
+                        if (world.getBlockState(newPos).getBlock() instanceof WetSpongeBlock)
+                        {
+                            world.setBlock(newPos, Blocks.SPONGE.defaultBlockState(), 3);
+                            world.levelEvent(2009, pos, 0);
+                            world.playSound((Player) null, pos, SoundEvents.FIRE_EXTINGUISH, SoundSource.BLOCKS, 1.0F, (1.0F + world.getRandom().nextFloat() * 0.2F) * 0.7F);
                         }
                     }
                 }
